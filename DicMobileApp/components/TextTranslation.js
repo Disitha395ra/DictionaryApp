8d870d18e14831d95cdf3631c53da244cf1d4e30
Disitha <disithar@gmail.com>
2024-12-29 16:07:48 +0530

    install pickerlibrary for dropdown menue

--- DicMobileApp/components/TextTranslation.js
+++ DicMobileApp/components/TextTranslation.js
@@ -1,9 +1,31 @@
 import { StyleSheet, View, ScrollView, Image, Dimensions } from "react-native";
-import { PaperProvider, Button,Text } from "react-native-paper";
+import { PaperProvider, Button, Text, TextInput } from "react-native-paper";
+import React from "react";
+
+
 export default function TextTranslation() {
+
+  const [text, setText] = React.useState("");
+
   return (
     <View>
-      <Text>Text Translation</Text>
+      <PaperProvider>
+        <Text variant="headlineMedium">Enter Your Text Here</Text>
+        <TextInput
+          label="Email"
+          value={text}
+          onChangeText={(text) => setText(text)}
+        />
+      </PaperProvider>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
